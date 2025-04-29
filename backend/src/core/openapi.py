16ab444e9051bbf09c643c3b1918b0030c1611e9
sansly <robertstepa017@gmail.com>
2025-04-29 22:10:40 +0300

    feat(open_api_params): add openapi path params detecting

--- backend/src/core/openapi.py
+++ backend/src/core/openapi.py
@@ -1,7 +1,7 @@
 from typing import Type, Dict, Optional
 import re
 
-from flask import Flask
+from flask import Flask, request
 from openapi_pydantic.v3.v3_0 import (
     Info,
     PathItem,
@@ -9,8 +9,10 @@ from openapi_pydantic.v3.v3_0 import (
     Response,
     Schema,
     Components,
-    RequestBody
+    RequestBody,
+    Parameter
 )
+
 from openapi_pydantic.v3.v3_0 import OpenAPI
 from openapi_pydantic.v3.v3_0.util import (
     PydanticSchema,
@@ -50,6 +52,8 @@ def generate_openapi_spec(app: Flask, title: str = "TeamSync API",
                                                                '_swagger_docs',
                                                                None)
 
+        path_params, openapi_url = _extract_flask_route_params(rule.rule)
+
         if not swagger_docs:
             continue
 
@@ -63,7 +67,8 @@ def generate_openapi_spec(app: Flask, title: str = "TeamSync API",
             operation = Operation(
                 description=swagger_docs.description,
                 responses=_create_responses(swagger_docs.responses,
-                                            components_schemas)
+                                            components_schemas),
+                parameters=_get_path_params_obj_list(path_params)
             )
 
             if swagger_docs.request_body:
@@ -72,7 +77,7 @@ def generate_openapi_spec(app: Flask, title: str = "TeamSync API",
 
             setattr(path_item, method, operation)
 
-            paths[rule.rule] = path_item
+            paths[openapi_url] = path_item
 
     open_api = OpenAPI(
         info=Info(title=title, version=version),
@@ -134,15 +139,27 @@ def _add_model_to_schemas(
 def _extract_flask_route_params(url):
     pattern = re.compile(r'<(?:(?P<type>\w+):)?(?P<name>\w+)>')
     args = {}
+    openapi_url = url
 
     for match in pattern.finditer(url):
         arg_type = match.group(
             "type") or "str"
         arg_name = match.group("name")
         args[arg_name] = arg_type
+        openapi_url = openapi_url.replace(
+            f"<{arg_type}:{arg_name}>" if arg_type != "str" else f"<{arg_name}>",
+            f"{{{arg_name}}}"
+        )
 
-    return args
+    return args, openapi_url
 
+
+def _get_path_params_obj_list(params):
+    params_obj_list = []
+    for param in params:
+        params_obj_list.append(Parameter(name=param, param_in="path", required=True))
+
+    return params_obj_list
 
 def register_openapi_spec_endpoint(app):
     """
@@ -151,4 +168,6 @@ def register_openapi_spec_endpoint(app):
     @app.route('/apispec_1.json')
     def get_spec():
 
-        return generate_openapi_spec(app, "api").model_dump_json(by_alias=True, exclude_none=True, indent=2)
+        return generate_openapi_spec(app, "api").model_dump_json(by_alias=True,
+                                                                 exclude_none=True
+                                                                 , indent=2)
