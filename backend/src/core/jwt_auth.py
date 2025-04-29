ff20de6bebfaaeab742ff26699af9d9c9290fc5c
sansly <robertstepa017@gmail.com>
2025-04-30 00:04:38 +0300

    feat(jwt): add jwt auth decorator

---
+++ backend/src/core/jwt_auth.py
@@ -0,0 +1,38 @@
+from functools import wraps
+
+from flask import request
+from werkzeug.exceptions import Unauthorized
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+
+from core.utils import get_func_instance_arg
+
+def jwt_auth(view):
+    """
+    Выполняет jwt аутентификацию пользователя исходя из токена
+    в заголовке Authorization
+    """
+    @wraps(view)
+    def wrapper(*args, **kwargs):
+        from users.models import User
+
+        _, arg_name = get_func_instance_arg(view, User)
+        if arg_name is None:
+            return view(*args, **kwargs)
+
+        try:
+            verify_jwt_in_request()
+
+            user_identity = get_jwt_identity()
+
+            user = User.query.filter_by(id=int(user_identity)).first()
+            kwargs[arg_name] = user
+            if not user:
+                raise Unauthorized("User not exist for this token or it expired")
+
+            return view(*args, **kwargs)
+
+        except Exception as e:
+            raise Unauthorized(str(e))
+
+    return wrapper
+
--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -2,8 +2,10 @@ from werkzeug.exceptions import NotFound
 from flask_jwt_extended import (create_access_token)
 from flask import Blueprint
 
+from core.jwt_auth import jwt_auth
 from core.pydantic_validation import pydantic_validation
 from core.swagger_docs import swagger_docs
+from users.models import User
 from users.schemas import UserSchemaLogin, TokenSchema
 from users.services import UserService
 
@@ -14,22 +16,18 @@ users_bp = Blueprint('users', __name__, url_prefix='/api/users')
 @users_bp.route("/signup", methods=["POST"])
 @swagger_docs(description="Регистрирует нового пользователя и возвращает токен",
               responses=[{201: TokenSchema}])
+@jwt_auth
 @pydantic_validation
-def signup(user_creds: UserSchemaLogin):
+def signup(user_creds: UserSchemaLogin, ):
     user = UserService.create(user_creds.password, user_creds.username)
-    access_token = create_access_token(identity=user.username)
+    access_token = create_access_token(identity=str(user.id))
     return TokenSchema(token=access_token), 201
 
 
 @users_bp.route("/login", methods=["POST"])
 @swagger_docs(description="Генерирует токен для пользователя",
               responses=[{200: TokenSchema}])
+@jwt_auth
 @pydantic_validation
-def login(user_creds: UserSchemaLogin):
-    user = UserService.get_by_pass_username(user_creds.password,
-                                            user_creds.username)
-    if user is None:
-        raise NotFound("Username or password incorrect")
-
-    access_token = create_access_token(identity=user.username)
-    return TokenSchema(token=access_token), 200
+def login():
+    return None, 200
