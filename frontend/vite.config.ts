83d472b344082adcb07fe59db5c1bfb2321d7dc7
sansly <robertstepa017@gmail.com>
2025-05-08 17:07:28 +0300

    feat(frontend): add docker and fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'esnext',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    allowedHosts: true
+  }
 });
